197c2a3aaadf0ec2d374dd147371caf987f8d64c
Lencse <leventeloki@gmail.com>
2021-07-01 22:48:40 +0200

    Better dev startup

--- dev/reverse-proxy/main.js
+++ dev/reverse-proxy/main.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
+const axios = require('axios')
 const express = require('express')
 const httpProxy = require('http-proxy')
 const morgan = require('morgan')
 const chalk = require('chalk')
 const assert = require('assert').strict
-
+const { setTimeout } = require('timers/promises')
 
 const MAIN_PORT = Number.parseInt(process.env.MAIN_PORT)
 const FRONTEND_PORT = Number.parseInt(process.env.FRONTEND_PORT)
@@ -13,6 +14,9 @@ const FRONTEND_PORT = Number.parseInt(process.env.FRONTEND_PORT)
 assert(MAIN_PORT > 0)
 assert(FRONTEND_PORT > 0)
 
+const WAIT_FOR = [
+    `http://localhost:${FRONTEND_PORT}`
+]
 
 const server = express()
 const proxy = httpProxy.createProxyServer({})
@@ -30,6 +34,24 @@ server.use((req, res) => {
     proxy.web(req, res, { target })
 })
 
-server.listen(MAIN_PORT, () => {
-    console.info(`Reverse proxy started: ${chalk.yellow.bold(`http://localhost:${MAIN_PORT}`)}`)
-})
+const main = async () => {
+
+    await Promise.all(
+        WAIT_FOR.map(async host => {
+            while (true) {
+                try {
+                    await axios.get(host)
+                    return
+                } catch (err) {
+                    await setTimeout(100)
+                }
+            }
+        })
+    )
+
+    server.listen(MAIN_PORT, () => {
+        console.info(`Reverse proxy started: ${chalk.yellow.bold(`http://localhost:${MAIN_PORT}`)}`)
+    })
+}
+
+main()
